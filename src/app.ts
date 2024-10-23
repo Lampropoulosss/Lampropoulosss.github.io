3346e4c3b213fb3d83a28a541cca35195596261d
Ioannis Lampropoulos <65957175+Lampropoulosss@users.noreply.github.com>
2024-10-24 00:47:52 +0300

    Added projects section

--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,15 @@ const footer_text = document.getElementById(
 ) as HTMLParagraphElement;
 
 footer_text.innerHTML = `&copy; ${new Date().getFullYear()} Ioannis Lampropoulos. All Rights Reserved.`;
+
+(window as any).scroll_to = (id: string) => {
+  const element = document.getElementById(id);
+  element?.scrollIntoView({ behavior: "smooth" });
+};
+
+const hamburgerIcon = document.getElementById("hamburger-icon");
+const hamburgerMenu = document.getElementById("hamburger-menu");
+
+hamburgerIcon?.addEventListener("click", () => {
+  hamburgerMenu?.classList.toggle("show");
+});
