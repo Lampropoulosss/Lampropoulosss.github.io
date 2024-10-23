15136a82912d56d73a4bc90eac2cfcf703affb7b
Ioannis Lampropoulos <65957175+Lampropoulosss@users.noreply.github.com>
2024-10-24 00:49:18 +0300

    Updates

--- index.6b815632.js
+++ index.6b815632.js
@@ -586,6 +586,17 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 },{}],"kuM8f":[function(require,module,exports) {
 const footer_text = document.getElementById("footer-paragraph");
 footer_text.innerHTML = `&copy; ${new Date().getFullYear()} Ioannis Lampropoulos. All Rights Reserved.`;
+window.scroll_to = (id)=>{
+    const element = document.getElementById(id);
+    element?.scrollIntoView({
+        behavior: "smooth"
+    });
+};
+const hamburgerIcon = document.getElementById("hamburger-icon");
+const hamburgerMenu = document.getElementById("hamburger-menu");
+hamburgerIcon?.addEventListener("click", ()=>{
+    hamburgerMenu?.classList.toggle("show");
+});
 
 },{}]},["1G5n2","kuM8f"], "kuM8f", "parcelRequire2041")
 
